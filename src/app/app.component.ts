4c01b463f81cc254596f9d75cb97e2e45d7a9bba
bnanaday <bnanaday@gmail.com>
2022-04-21 23:08:54 -0500

    get components working with buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+
+export type EditorType = 'list-contact' | 'add-contact' | 'edit-contact';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  editor: EditorType = 'list-contact';
 
+  get showListContact() {
+    return this.editor === 'list-contact';
+  }
+
+  get showAddContact() {
+    return this.editor === 'add-contact';
+  }
+
+  get showEditContact() {
+    return this.editor === 'edit-contact';
+  }
+
+  toggleEditor(type: EditorType) {
+    this.editor = type;
+  }
 }
